1b635f07a83b3c064cc5273f19e20a0d2b429c92
LidaDavydova <davydoval2005@gmail.com>
2025-06-29 18:12:45 +0300

    add: ready RAG with top 5 and add to bot

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -4,14 +4,135 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+import ollama
+
+import faiss
+import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from pathlib import Path
+from collections import defaultdict
 
 load_dotenv()
 TOKEN = os.environ['TOKEN']
+
+model = SentenceTransformer('cointegrated/rubert-tiny2')
+index = faiss.read_index("../embeddings/embeddings.index")
+
+chunk_files = sorted(Path('..').glob('dataset/preproc_data*.parquet'))
+dfs = [pd.read_parquet(f) for f in chunk_files]
+df = pd.concat(dfs, ignore_index=True)
+
+df[['classifier_code', 'classifier_name']] = df['classifierByIPS'].str.split('$', n=1, expand=True)
+df['classifier_level2'] = df['classifier_code'].str.extract(r'^(\d{3}\.\d{3})')
+
+
+template = '''Контекст: {context}
+Исходя из контекста, ответьте на следующий вопрос:
+Вопрос: {query}
+Предоставьте ответ только на основе предоставленного контекста, без использования общих знаний. Ответ должен быть непосредственно взят из предоставленного контекста.
+Пожалуйста, исправьте грамматические ошибки для улучшения читаемости.
+Если в контексте нет информации, достаточной для ответа на вопрос, укажите, что ответ отсутствует в данном контексте.
+Пожалуйста, включите источник информации в качестве ссылки, поясняющей, как вы пришли к своему ответу.'''
 
 
 bot = Bot(TOKEN)
 dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
+
+
+def generate_llm_response(query, top_results, template, model_name="gemma3:1b"):
+    # generate contex of top 5
+    context = ""
+    sources = []
+    
+    for i, res in enumerate(top_results, 1):
+        context += f"[[Источник {i}]]\n{res['text']}\n\n"
+        sources.append({
+            "index": res['index'],
+            "similarity": res['similarity'],
+            "classifier": res['classifier'],
+            'heading': res['heading'],
+            'text': res['text']
+        })
+        
+    prompt = template.format(context=context, query=query)
+    
+    # generate responce from ollama 
+    response = ollama.generate(
+        model=model_name,
+        prompt=prompt,
+        options={
+            "temperature": 0.3,
+            "num_ctx": 10000 # context size
+        }
+    )
+    
+    answer = response['response']
+    
+    answer += "\n\n---\nRetrieval laws:\n"
+    for src in sources:
+        answer += (f"- [ID: {src['index']}] cos similarity: {src['similarity']:.3f},\n"
+                  f"classifier code: {src['classifier']}, 'low heading: {src['heading']}\n")
+
+    return answer
+
+def RAG(query):
+    query_vec = model.encode([query]) # encode
+    query_vec = query_vec / np.linalg.norm(query_vec) # normalyze
+
+    faiss.normalize_L2(query_vec)
+
+    k = 50
+    # find the closest
+    similarities, indices = index.search(query_vec, k)
+
+    # filter resilt from top 50 to top 5
+    results = []
+    for idx, sim in zip(indices[0], similarities[0]):
+        if idx == -1:  # if idx not exist, skip
+            continue
+        
+        row = df.iloc[idx]
+        classifier = str(row['classifier_level2']).strip() if pd.notna(row['classifier_level2']) else "UNKNOWN"
+        
+        # add if not UNKNOWN
+        if classifier != "UNKNOWN":
+            results.append({
+                'index': idx,
+                'similarity': sim,
+                'classifier': row['classifier_code'],
+                'classifier_level2': classifier,
+                'text': row['textIPS'],
+                'heading': row['headingIPS']
+            })
+
+    # group by classifier_level2 (2 numbers from classifier_code)
+    grouped = defaultdict(list)
+    for res in results:
+        grouped[res['classifier_level2']].append(res)
+
+    top_results = []
+    # take from each grouped 1 with max similarity
+    if len(grouped) >= 5:
+        for cls in sorted(grouped.keys(), key=lambda x: max(y['similarity'] for y in grouped[x]), reverse=True)[:5]:
+            best_in_cls = max(grouped[cls], key=lambda x: x['similarity'])
+            top_results.append(best_in_cls)
+
+    # take from each grouped 2 with max similarity
+    else:
+        candidates = []
+        for cls, items in grouped.items():
+            items_sorted = sorted(items, key=lambda x: -x['similarity'])
+            candidates.extend(items_sorted[:2])
+        # return only top 5
+        top_results = sorted(candidates, key=lambda x: -x['similarity'])[:5]
+
+    print("Топ-5 результатов с учетом классов:")
+    for i, res in enumerate(top_results, 1):
+        print(f"{i}. Схожесть: {res['similarity']:.3f} | Класс: {res['classifier_level2']}")
+        print(f"Текст: {res['text']}")
+    return top_results
 
 @dp.message(Command("start"))
 async def start_handler(message: types.Message):
@@ -24,12 +145,17 @@ async def start_handler(message: types.Message):
 async def handle_user_message(message: types.Message):
     query = message.text
 
-    closest = ['a', 'b', 'very\nlong\nlaw\naaa\na'] #как-то находим самые близкие к запросу документы
-    extra_text = ['Да', "Нет", "Наверное"] #как-то обрабатываем LLM
+    top_res = RAG(query)
+    answer = generate_llm_response(query, top_res, template, model_name="gemma3:1b")
+    
 
-
-    response = '\n\n'.join([f"**>{closest[i].replace('\n', '\n>')}||\nБолее простым языком:\n{extra_text[i]}" for i in range(len(closest))])
-    await message.answer(response, parse_mode='MarkdownV2')
+    # response = '\n\n'.join([
+    #     "**>{}||\nБолее простым языком:\n{}".format(
+    #         closest[i].replace('\n', '\n>'), extra_text[i]
+    #     )
+    #     for i in range(len(closest))
+    # ])
+    await message.answer(answer)
 
 
 async def main():
